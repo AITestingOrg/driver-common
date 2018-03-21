852a7bc1459dade9ed3bc0e3762cdba1729979ac
Lam Pui Chan <lam_chan@ultimatesoftware.com>
2018-03-21 11:42:58 -0400

    Added new fields to DriverCreated, added NotFoundException class

--- src/main/java/org/aitesting/microservices/driver/common/events/DriverCreatedEvent.java
+++ src/main/java/org/aitesting/microservices/driver/common/events/DriverCreatedEvent.java
@@ -7,20 +7,40 @@ import org.aist.libs.eventsourcing.configuration.eventstore.Name;
 @Name("DriverCreatedEvent")
 public class DriverCreatedEvent extends Event {
 
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String phone;
+    private String currentLocation;
     private String license;
     private boolean available;
 
-    public DriverCreatedEvent(UUID id, String license, boolean available) {
+    public DriverCreatedEvent(UUID id, String firstName, String lastName, String address, String email, String phone, String currentLocation, String license, boolean available) {
         super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.currentLocation = currentLocation;
         this.license = license;
         this.available = available;
     }
 
-    public String getLicense() {
-        return license;
-    }
+    public String getFirstName() { return firstName; }
 
-    public boolean isAvailable() {
-        return available;
-    }
+    public String getLastName() { return lastName; }
+
+    public String getAddress() { return address; }
+
+    public String getEmail() { return email; }
+
+    public String getPhone() { return phone; }
+
+    public String getCurrentLocation() { return currentLocation; }
+
+    public String getLicense() { return license; }
+
+    public boolean isAvailable() { return available; }
 }
